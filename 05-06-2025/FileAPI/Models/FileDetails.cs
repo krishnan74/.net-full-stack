75abb56d198485fb584cfefdc68bcd9712328080
krishnan74 <divyakrishnan.r74@gmail.com>
2025-06-06 14:06:35 +0530

    refactor - fixed wrong import

--- 05-06-2025/FileAPI/Models/FileDetails.cs
+++ 05-06-2025/FileAPI/Models/FileDetails.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Notify.Models
+namespace FileAPI.Models
 {
     public class FileDetails
     {
