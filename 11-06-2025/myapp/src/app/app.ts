44bebcc403587610b2de81efe01e410ad8efd49b
krishnan74 <divyakrishnan.r74@gmail.com>
2025-06-11 14:48:58 +0530

    feat: Day 28 Angular ( Created product model and service for fetching product data from external API )

---
+++ 11-06-2025/myapp/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Product } from './product/product';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Product],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'myapp';
+}
