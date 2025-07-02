51c2a1780b0a59a61953a9c2ed2472c126bda996
krishnan74 <divyakrishnan.r74@gmail.com>
2025-07-02 09:38:44 +0530

    feat: Dockerize dotnet webapi along with postgres DB using docker compose

---
+++ 01-07-2025/FileAPI/Migrations/20250605070339_init.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FileAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "fileDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FileName = table.Column<string>(type: "text", nullable: false),
+                    FileData = table.Column<byte[]>(type: "bytea", nullable: false),
+                    FileType = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_fileDetails", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "fileDetails");
+        }
+    }
+}
---
+++ 01-07-2025/FileAPI/Program.cs
@@ -0,0 +1,76 @@
+using System.Text;
+using FileAPI.Contexts;
+using FileAPI.Interfaces;
+using FileAPI.Models;
+using FileAPI.Misc;
+using FileAPI.Repositories;
+using FileAPI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "File API", Version = "v1" });
+});
+
+
+builder.Services.AddControllers()
+                .AddJsonOptions(opts =>
+                {
+                    opts.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+                    opts.JsonSerializerOptions.WriteIndented = true;
+                });
+
+builder.Services.AddDbContext<FileManagerContext>(opts =>
+{
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+#region  Repositories
+builder.Services.AddTransient<IRepository<int, FileDetails>, FileRepository>();
+
+#endregion
+
+#region Services
+builder.Services.AddTransient<IFileService, FileService>();
+
+#endregion
+
+#region CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://127.0.0.1:5500")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+#endregion
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseCors();
+app.MapHub<NotificationHub>("/notificationHub");
+
+app.MapControllers();
+
+app.Run();
+
