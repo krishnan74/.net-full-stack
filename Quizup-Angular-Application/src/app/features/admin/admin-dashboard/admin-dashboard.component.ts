4ca43c53478899de48023e48d3f96304c730d186
krishnan74 <divyakrishnan.r74@gmail.com>
2025-07-08 12:52:50 +0530

    refactor:  Update landing content, styles and structure

--- Quizup-Angular-Application/src/app/features/admin/admin-dashboard/admin-dashboard.component.ts
+++ Quizup-Angular-Application/src/app/features/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { BreadcrumbsComponent } from '../../../shared/components/breadcrumbs/breadcrumbs.component';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.css'],
-  imports: [RouterLink, BreadcrumbsComponent],
+  imports: [RouterLink],
 })
 export class AdminDashboardComponent {}
