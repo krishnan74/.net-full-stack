4572462f34fbaa321db63968572cc12e8fd2971a
krishnan74 <divyakrishnan.r74@gmail.com>
2025-07-23 16:06:44 +0530

    feat: Add function to retrieve subjects by student ID

--- Quizup-Dotnet-Backend/QuizupAPI/SQL/utility-functions.sql
+++ Quizup-Dotnet-Backend/QuizupAPI/SQL/utility-functions.sql
@@ -22,4 +22,27 @@ $$ LANGUAGE plpgsql;
 
 SELECT * FROM get_questions_by_teacher_id(1);
 
+CREATE OR REPLACE FUNCTION get_subjects_by_student_id(p_student_id BIGINT)
+RETURNS TABLE(
+	"Id" BIGINT,
+	"Name" TEXT,
+	"Code" TEXT, 
+	"CreatedAt" TIMESTAMP WITH TIME ZONE,
+	"UpdatedAt" TIMESTAMP WITH TIME ZONE,
+	"IsDeleted" BOOLEAN
+)
+AS $$
 
+BEGIN
+    RETURN QUERY
+    SELECT 
+        s.*
+    FROM students st
+    JOIN classes cl ON st."ClassId" = cl."Id"
+    JOIN "classSubjects" cs ON cs."ClassId" = cl."Id"
+    JOIN subjects s ON cs."SubjectId" = s."Id"
+    WHERE st."Id" = p_student_id;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM get_subjects_by_student_id(1);
